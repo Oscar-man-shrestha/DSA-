64a92cd3a363206af63b6bddf0c39c28e17c8d0c
Oscar-man-shrestha <bl.sc.u4cse24267@bl.students.amrita.edu>
2025-06-06 18:39:20 +0545

    first commit

---
+++ typeConversion.java
@@ -0,0 +1,17 @@
+// Type Conversion || widening || implicit 
+
+//Conversion happens when :
+//a. type compatible
+//b. destination type > source type
+
+//  byte -> short -> int -> float -> long -> double
+
+public class typeConversion {
+    public static void main(String[] args) {
+        int a= 25;
+        long b= a;
+        System.out.println(b);
+    }
+}
+
+
