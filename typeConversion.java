2c306dba423c62a1d384fa3a99795009cdfb6fa0
Oscar-man-shrestha <bl.sc.u4cse24267@bl.students.amrita.edu>
2025-06-09 21:59:22 +0545

    saved

--- typeConversion.java
+++ typeConversion.java
@@ -1,17 +1,62 @@
-// Type Conversion || widening || implicit 
+// # Type Conversion || widening || implicit 
 
 //Conversion happens when :
 //a. type compatible
 //b. destination type > source type
 
-//  byte -> short -> int -> float -> long -> double
+//  byte -> short -> int -> float -> long -> double 
 
-public class typeConversion {
-    public static void main(String[] args) {
-        int a= 25;
-        long b= a;
-        System.out.println(b);
-    }
-}
+// Brilliant Scientists Invent Fantastic Laboratory Devices
+
+// public class typeConversion {
+//     public static void main(String[] args) {
+//         int a= 25;
+//         long b= a;
+//         System.out.println(b);
+//     }
+// }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Type casting || narrowing || explicit
+
+
+// public class typeConversion {
+//     public static void main(String[] args) {
+     
+// float a = 9.99f;
+// int b = (int)a;
+
+// char ch='a';
+// int num = ch;
+
+// System.out.println(b);
+// System.out.println(num);
+
+//     }
+// }
+
+///------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// Type Promotion
+
+
+
+// public class typeConversion {
+//     public static void main(String[] args) {
+     
+// byte b = 5;
+// byte a = (byte)(b*2);
+// System.out.println(a);
+
+//     }
+// }
+
+
+
+
+
 
 
