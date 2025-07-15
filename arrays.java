c465f46c06ffb35a19b312b4e4342d303ed9ec94
Oscar-man-shrestha <bl.sc.u4cse24267@bl.students.amrita.edu>
2025-07-16 01:27:06 +0530

    arrays

--- arrays.java
+++ arrays.java
@@ -240,17 +240,36 @@
 // }
 
 // ================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
-//                             Max subarray sum
-
-
-import java.util.Scanner;
+//                             Max subarray sum (Brute force)
 
 public class arrays {
 
-   public static void subarrays(int numbers[]) {
-   
+   public static void maxSubArrays(int numbers[]) {
+    int currentSum=0;
+    int maxSum=Integer.MIN_VALUE;
+
+    for(int i=0;i<numbers.length;i++){
+        int start = i;
+        for(int j=i;j<numbers.length;j++){
+            int end = j;
+            currentSum=0;
+            for(int k=start;k<=end;k++){
+               currentSum +=numbers[k];
+ }
+ System.out.println(currentSum);
+               if(maxSum< currentSum){
+                maxSum=currentSum;
+               }
+       
+       
+        
+        }
+       
+    }
+    System.out.println("Max sum = "  + maxSum);
 }
     public static void main(String[] args) {
-    
+    int numbers[] = {1,-2,6,-1,3};
+   maxSubArrays(numbers);
     }
 }
